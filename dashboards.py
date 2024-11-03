5b803912e237f26fbba0ee6235231ac37198db52
rodrigonex <rodrigoe923@gmail.com>
2024-11-03 12:16:16 -0300

    first commit

---
+++ dashboards.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+# Com uma visão mensal
+df = pd.read_csv("supermarket_sales.csv", sep=";",
+                 decimal=",", encoding='ISO-8859-1')
+
+df["Date"] = pd.to_datetime(df["Date"])
+df = df.sort_values("Date")
+
+df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+month = st.sidebar.selectbox("Mês", df["Month"].unique())
+
+df_filtered = df[df["Month"] == month]
+
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
+
+# faturamento por unidade
+fig_date = px.bar(df_filtered, x="Date", y="Total",
+                  color="City", title="Faturamento por dia")
+col1.plotly_chart(fig_date, use_container_width=True)
+
+# tipo de produtos mais vendido, contribuição por filial
+dif_prod_date = px.bar(df_filtered, x="Date", y="Product line",
+                       color="City", title="Faturamento por produto", orientation="h")
+col2.plotly_chart(dif_prod_date, use_container_width=True)
+
+city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
+fig_city = px.bar(city_total, x="City", y="Total",
+                  title="Faturamento por filial")
+col3.plotly_chart(fig_city, use_container_width=True)
+
+# Desempenho das forma de pagamento
+fig_kind = px.pie(df_filtered, values="Total", names="Payment",
+                  title="Faturamento por tipo de pagamento")
+col4.plotly_chart(fig_kind, use_container_width=True)
+
+# Como estão as avaliações das filias?
+city_rating_total = df_filtered.groupby(
+    "City")[["Rating"]].mean().reset_index()
+fig_rating = px.bar(city_rating_total, y="Rating", x="City",
+                    title="Avaliação")
+col5.plotly_chart(fig_rating, use_container_width=True)
